741bc25b17df6876f667fa61dc9bd534aee7bd0c
LyTrungHai <trunghai1610@gmail.com>
2022-04-01 20:51:12 +0700

    'complete'

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import { isAuthenticated, logout } from "../../auth";
 // import { useSelector } from "react-redux";
 import axios from "axios";
 import "./Home.css";
+import video from "../../assets/Lions 101 _ Nat Geo Wild.mp4";
 
 export default function Home() {
   const { email } = isAuthenticated();
@@ -22,15 +23,24 @@ export default function Home() {
     fetchAnimalList();
   }, []);
 
+  // const transitions = useTransition(items, {
+  //   from: { opacity: 0 },
+  //   enter: { opacity: 1 },
+  //   leave: { opacity: 0 },
+  // });
   const GetListAnimal = () => {
     return listAnimal.map((element) => {
       return (
         <div className="card" key={element.id}>
-          <img src={element.avatar} alt="Avatar" style={{ width: "100%" }} />
+          <div className="img-animal">
+            <img src={element.avatar} alt="Avatar" style={{ width: "100%" }} />
+          </div>
           <div className="content">
             <span>{element.id}</span>
             <h4>
-              <b>{element.name}</b>
+              <b>
+                <a href="">{element.name}</a>
+              </b>
             </h4>
           </div>
         </div>
@@ -40,17 +50,38 @@ export default function Home() {
 
   return (
     <div className="bodyHome">
-      <div className="header ">
-        <h1>
-          Welcome <span>{email}</span>
-        </h1>
+      <nav className="header ">
+        <h1>Animal </h1>
         <button className="btn-logout" onClick={logout}>
           Logout
         </button>
-      </div>
-      <div className="container">
-        <GetListAnimal />
+      </nav>
+      <div className="background">
+        <h1 className="welcome">Welcome {email} </h1>
+        <div className="line"></div>
+        <h3 className="animals">ANIMALS</h3>
+        <div className="container">
+          <GetListAnimal />
+        </div>
       </div>
     </div>
   );
 }
+
+// <div>
+//   <div className="background">
+//     <div className="video-zone">
+//       <video autoPlay muted loop id="myVideo">
+//         <source src={video} type="video/mp4" />
+//       </video>
+//     </div>
+//     <div className="contentVideo">
+//       <div className="text-video">
+//         <h1>
+//           wellcome <span>{email}</span>
+//         </h1>
+//         <p> to animal page</p>
+//       </div>
+//     </div>
+//   </div>
+// </div>;
